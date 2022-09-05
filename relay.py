c0a6a431d913d079c91c9b08e20209777a33a4a7
geoperros <geoperros@hotmail.com>
2022-09-05 18:53:38 +0300

    Relay: Change of time and troubleshooting the relay not going into the while loop.

--- relay.py
+++ relay.py
@@ -18,7 +18,7 @@ def get_temp(dev_file):
         temperature = contents[-1][index+2:]
         cels =float(temperature)/1000
         return cels
-# Initial test of the relay to inform the user that the relay is working as intended.
+'''
 if __name__ == '__main__':
     try:
         motor_on(channel)
@@ -28,20 +28,49 @@ if __name__ == '__main__':
         GPIO.cleanup()
     except KeyboardInterrupt:
         GPIO.cleanup()
-
+'''
 temp = get_temp("/sys/bus/w1/devices/28-3c01a816dc0b/w1_slave")
-while temp>10:
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(channel, GPIO.OUT)
-    # By setting this if statement we control the lowest temperature that the fan will work.
+while True:
     if temp > 23.5:
+        channel = 21
         motor_on(channel)
-        time.sleep(60)
+        time.sleep(180)
         temp = get_temp("/sys/bus/w1/devices/28-3c01a816dc0b/w1_slave")
-        GPIO.cleanup()
     else:
+        channel = 21
         motor_off(channel)
-        time.sleep(60)
+        time.sleep(180)
         temp = get_temp("/sys/bus/w1/devices/28-3c01a816dc0b/w1_slave")
-        GPIO.cleanup()
+#def get_temp(dev_file):
+#    f = open(dev_file,"r")
+#    contents = f.readlines()
+#    f.close()
+#    index = contents[-1].find("t=")
+#    if index != -1 :
+#        temperature = contents[-1][index+2:]
+#        cels =float(temperature)/1000
+#        return cels
+# Parse command line parameters.
+#sensor_args = { '11': Adafruit_DHT.DHT11,
+#                '22': Adafruit_DHT.DHT22,
+#                '2302': Adafruit_DHT.AM2302 }
+#if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
+#    sensor = sensor_args[sys.argv[1]]
+#    pin = sys.argv[2]
+#else:
+#    print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
+#    print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
+#    sys.exit(1)')
+#humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+#while 0<1:
+#
+#       if humidity is not None and temperature is not None:
+#                print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, >
+#                if __name__ == "__main__":
+#                        temp = get_temp("/sys/bus/w1/devices/28-3c01a816dc0b/w1>
+#                        print(temp)
+#                time.sleep(1)
+#        else:
+#                print('Failed to get reading. Try again!')
+#                sys.exit(1)
 
